6ee3415c49275a64104b8f38733885bbbf95291f
Benjamin <migom@hotmail.be>
2023-09-21 14:07:14 +0200

    arrays oef 07

--- oefenbundel/03_arrays/oef07/js/index.js
+++ oefenbundel/03_arrays/oef07/js/index.js
@@ -1,6 +1,7 @@
 let array1 = [];
 let array2 = [];
-let array3 = [];
+let even = [];
+let som = 0;
 let max = Number(prompt("Geef het max nummer in: "));
 
 let randomNumber = Math.floor(Math.random()*max);
@@ -15,6 +16,18 @@ document.write(`${array1} <br>`);
 document.write(`${array2}<p>`);
 
 for (let j = 0; j < array1.length; j++){
-    array3.push(array1[j] + array2[j]);
-    document.write(`${array1[j]} + ${array2[j]} = ${array3[j]}<br>`)
+    if (array1[j]%2 ===0){
+        even.push(array1[j]);
+    }else{
+        som+= array1[j];
+    }
 }
+for (let k = 0; k < array2.length; k++){
+    if (array2[k]%2 ===0){
+        even.push(array2[k]);
+    }else{
+        som+= array2[k];
+    }
+}
+document.write(even + "<br>");
+document.write("De som van de oneven getallen is " + som + "<br>");
